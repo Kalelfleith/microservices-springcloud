952e176f1c226b70f9acd3ede1d704ea72f0271c
kalelfleith <kalelfleith03@gmail.com>
2022-03-09 23:01:48 -0300

    controllerRest initial service

--- src/main/java/one/digitalinovationone/experts/productcatalog/controller/ProductController.java
+++ src/main/java/one/digitalinovationone/experts/productcatalog/controller/ProductController.java
@@ -1,11 +1,22 @@
 package one.digitalinovationone.experts.productcatalog.controller;
 
+import one.digitalinovationone.experts.productcatalog.model.Product;
+import one.digitalinovationone.experts.productcatalog.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/product")
 public class ProductController {
 
-	
+	@Autowired
+	private ProductRepository productRepository;
+
+	@RequestMapping(method = RequestMethod.POST)
+	Product create(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
 }
