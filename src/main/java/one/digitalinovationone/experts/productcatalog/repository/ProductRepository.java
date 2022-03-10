b478d82f3ec860456311515c36018004832e9e81
kalelfleith <kalelfleith03@gmail.com>
2022-03-09 22:58:00 -0300

    Repository and ControllerRest inicital structure

---
+++ src/main/java/one/digitalinovationone/experts/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package one.digitalinovationone.experts.productcatalog.repository;
+
+import one.digitalinovationone.experts.productcatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+
+
+}
